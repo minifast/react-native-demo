86258d980b0322f7f07d9c4cdca4630d01e39367
CJ Bryan <cj@ministryofvelocity.com>
2017-03-02 14:40:22 -0800

    Move the feature specs into their own folder.

---
+++ spec/features/hello_world_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe 'Welcome to React', type: :feature do
+  it 'displays the welcome message' do
+    text('Welcome to React Native!')
+  end
+
+  # An example of a spec failure.
+  # it 'mentions kangaroos' do
+  #   text('Kangaroos are great.')
+  # end
+end
--- spec/hello_world_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe 'Welcome to React', type: :feature do
-  it 'displays the welcome message' do
-    expect(text('Welcome to React Native!')).not_to be_nil
-  end
-end
